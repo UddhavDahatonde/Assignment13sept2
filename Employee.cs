79d2f8a5c32da0a58cdb09396885ea1f026c8d32
Uddhav Dahatonde <dahatondeuddhava@gmail.com>
2022-09-14 10:49:39 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13sept2
+{
+    public class Employee
+    {
+        string Name;
+        int id;
+        double salary;
+        public void AssignValue()
+        {
+            Name = "Uddhav Dahatonde";
+            id = 123;
+            salary = 50000.0;
+        }
+        public string DisplayValue()
+        {
+            return $"Name==>{Name} id==>{id} salary ==>{salary}";
+        }
+    }
+}
